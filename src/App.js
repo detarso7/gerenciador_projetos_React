fc30116ec959927bc5c1fa7a97a91654f7e93109
Saulo de Tarso <de_tarso7@hotmail.com>
2023-03-21 18:37:57 -0300

    Criando a Home e Componente de Botão

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Projects from "./components/pages/Projects";
+import NewProjects from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
 import Navabar from "./components/layout/Navbar";
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/company" element={<Company/>}/>
         <Route exact path="/projects" element={<Projects/>}/>
         <Route exact path="/contact" element={<Contact/>}/>
+        <Route exact path="/newprojects" element={<NewProjects/>}/>
       </Routes>
       </Container>
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,15 @@
+import Styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkBottun'
+
 function Home(props){
     return(
-    <>
-    <h2>Home</h2>
-    </>
+        <section className={Styles.home_container}>
+            <h1>Bom vindo ao <span>Projetise</span></h1>
+            <p>Comece a gerir seus projetos agora mesmo</p>
+            <LinkButton to="/newprojects" text="Criar Projeto"/>
+            <img src={savings} alt="Gegenciador de Projetos"/>
+        </section>
     )
     
 }
