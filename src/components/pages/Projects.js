43d9025efeee58eaee92ddb495e04e2bce1d49b2
Saulo de Tarso <de_tarso7@hotmail.com>
2023-03-25 17:24:07 -0300

    Preparando pg Projects para receber os projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,29 @@
 import Message from "./Message"
 import { useLocation } from 'react-router-dom'
+import Container from "../layout/Container";
+
+import styles from './Projects.module.css'
+import LinkButton from "../layout/LinkBottun";
 
 function Projects(props){
 
     const location = useLocation()
     let message = ''
+    
     if (location.state) {
       message = location.state.message
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}> 
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newprojects" text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     )
     
